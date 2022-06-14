f89d93023fa9184727a8cbed1bfac57ba161419f
Roman <roman.cabalum@gmail.com>
2022-06-15 01:53:46 +0800

    Added StartScreen UI Elements to be added: > Coin toss animation > Reset/Settings Button to left > Time to right

---
+++ scripts/startscreen.js
@@ -0,0 +1,52 @@
+import renderApp from "./gamelogic.js";
+
+const appElement = document.getElementById("app");
+
+const body = document.querySelector("body");
+
+const makeOverlay = () => {
+  const overlay = document.createElement("div");
+  overlay.classList.add("overlay");
+  overlay.classList.add("hidden");
+
+  body.append(overlay);
+  return overlay;
+};
+
+const makeModal = () => {
+  const overlay = makeOverlay();
+  const modal = document.createElement("div");
+  modal.classList.add("modal-start");
+
+  //header
+  const h3 = document.createElement("h3");
+  h3.textContent = "Welcome to Chess!";
+  modal.append(h3);
+
+  //description
+  const p = document.createElement("p");
+  p.textContent = "Who plays first?";
+  modal.append(p);
+
+  //buttons
+  const coinToss = document.createElement("button");
+  coinToss.textContent = "Coin Toss";
+  let whoStartsFirst;
+
+  coinToss.onclick = () => {
+    whoStartsFirst = Math.floor(Math.random() * 2) + 1 === 1 ? true : false;
+    console.log(whoStartsFirst);
+    renderApp(appElement, whoStartsFirst);
+    modal.remove();
+  };
+
+  modal.append(coinToss);
+
+  overlay.append(modal);
+};
+
+const startScreen = () => {
+  return makeModal();
+};
+
+export default startScreen();

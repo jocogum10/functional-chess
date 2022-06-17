c1760edf9e93c60ec3142a2287bc0c24614836d8
Roman Cabalum <roman.cabalum@gmail.com>
2022-06-17 19:48:05 +0800

    added possible moves list to be rendered

--- scripts/checkMate.js
+++ scripts/checkMate.js
@@ -1,32 +1,39 @@
 export default function checkMate(gameboard, Checkobj) {
   let possibleMoves = 0;
+  let canBeMoved = [];
   const kingRow = Checkobj.whereKing[0];
   const kingCol = Checkobj.whereKing[1];
 
-  if (
-    gameboard[kingRow][kingCol + 1].type === "bq" ||
-    gameboard[kingRow][kingCol + 1].type === "br" ||
-    gameboard[kingRow][kingCol + 1].type === "bp" ||
-    gameboard[kingRow][kingCol - 1].type === "br" ||
-    gameboard[kingRow][kingCol - 1].type === "bq" ||
-    gameboard[kingRow][kingCol - 1].type === "br"
-  ) {
-    possibleMoves++;
-  }
-  gameboard[Checkobj.whereKing[0] + 1].forEach((cell) => {
-    //if (cell.type === "br" || cell.type === "bq") possibleMoves++;
+  gameboard[kingRow].forEach((cell) => {
+    if (cell.type === "bq" || cell.type === "br" || cell.type === "bp") {
+      if (Math.abs(cell.column - kingCol === 1)) {
+        possibleMoves++;
+        canBeMoved.push(cell);
+      }
+    }
+    if (cell.type === "bh") {
+      if (Math.abs(cell.column - kingCol === 3)) {
+        possibleMoves++;
+        canBeMoved.push(cell);
+      }
+    }
+  });
 
+  gameboard[kingRow + 1].forEach((cell) => {
+    if (cell.type === "br" || cell.type === "bq") possibleMoves++;
     if (cell.type === "bp") {
       if (
         Math.abs(kingCol - Checkobj.pieceWhichChecked.column) < cell.column &&
         Checkobj.pieceWhichChecked.column > cell.column
       ) {
-        console.log(cell);
         possibleMoves++;
+        canBeMoved.push(cell);
       }
     }
   });
 
   console.log("possible moves: ", possibleMoves);
+  console.log(canBeMoved);
+
   return possibleMoves;
 }
